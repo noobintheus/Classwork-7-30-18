57deeab79468f955558ad16c51578c3b701626be
noobintheus <41026504+noobintheus@users.noreply.github.com>
2018-08-30 21:39:50 -0500

    Update App.js
    I sent this up and instead of pulling it or making two extra, im just gonna change them on here. itss just copy and pasting with a few word changes

--- glass/src/App.js
+++ glass/src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import TwoPlayerScoreKeeper from "./TwoPlayerScoreKeeper";
 import ScoreButton from "./ScoreButton";
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
           <TwoPlayerScoreKeeper/>
           <ScoreButton/>
-
+<kindle/> <DisplayBooks/>
+      <AlienCharacter/> <DisplayAliens/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title"></h1>
